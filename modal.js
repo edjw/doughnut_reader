ccd7d9f3ecd9aeab02981bcbfcd4f7d70a2b26c2
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2019-06-17 22:52:17 +0100

    adding top level comments as a modal

---
+++ modal.js
@@ -0,0 +1,23 @@
+function attachModalListeners() {
+	document.getElementById("close_modal").addEventListener("click", toggleModal);
+	document.getElementById("overlay").addEventListener("click", toggleModal);
+}
+
+function detachModalListeners() {
+	document.getElementById("close_modal").removeEventListener("click", toggleModal);
+	document.getElementById("overlay").removeEventListener("click", toggleModal);
+}
+
+function toggleModal() {
+
+	const currentVisibility = document.getElementById("modal").classList;
+
+	if (currentVisibility.contains("d-none")) {
+		currentVisibility.remove("d-none");
+		attachModalListeners();
+	}
+	else {
+		currentVisibility.add("d-none");
+		detachModalListeners();
+	}
+}
